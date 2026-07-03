bee29d91bc260da280a3dbbb08061716b1f70a9a
Winkesa <winfreywekesa148@gmail.com>
2026-07-03 14:36:41 +0300

    Mock data for the inventory

---
+++ inventory.py
@@ -0,0 +1,15 @@
+inventory = [
+    {
+        "status": 1,
+        "product_name": "Organic Almond Milk",
+        "brands": "Silk",
+        "ingredients_text": "Filtered water, almonds, cane sugar, ..."
+            
+    },
+    {
+        "status": 2,
+        "product_name": "Gluten-Free Bread",
+        "brands": "Udi's",
+        "ingredients_text": "Water, brown rice flour, tapioca starch, ..."
+    }
+]
