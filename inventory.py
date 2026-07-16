7137b583809f684d5af186c8e6c5efab2977375b
Winkesa <winfreywekesa148@gmail.com>
2026-07-16 13:59:26 +0300

    new changes

--- inventory.py
+++ inventory.py
@@ -1,5 +1,3 @@
-import requests
-
 inventory = [
     {
         "status": 1,
@@ -15,38 +13,4 @@ inventory = [
         "ingredients_text": "Water, brown rice flour, tapioca starch, ..."
     }
 ]
-
-def search_inventory(product_name):
-    url =  "https://world.openfoodfacts.org/cgi/search.pl"
-
-    params={
-            "search_terms": product_name,
-            "search_simple": 1,
-            "action": "process",
-            "json": 1
-        }
-    
-    response = requests.get(url, params=params)
-
-    if response.status_code != 200:
-        return None
-    
-    data = response.json()
-
-    products = data.get("products", [])
-
-    if not products:
-        return None
-    
-    results = []
-
-    for product in products[:10]:
-        results.append({
-        "status": "new_status",
-        "product_name": data.get('product_name'),
-        "brands": data.get('brands'),
-        "ingredients_text": data.get('ingredients_text')
-        })
-
-    return results
 
